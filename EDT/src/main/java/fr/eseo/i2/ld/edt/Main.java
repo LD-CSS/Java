228c993604d2e689bb70ecb2333db263788fd95f
ClemLB <clem.leborgne@gmail.com>
2019-12-20 08:40:38 +0100

    Création de la vue principale

--- EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
+++ EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
@@ -1,8 +1,14 @@
 package fr.eseo.i2.ld.edt;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+public class Main extends Application {
+
+	// Appellé quand le Main est lancé
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 
