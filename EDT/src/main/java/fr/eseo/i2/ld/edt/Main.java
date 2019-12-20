f3df725a1c5fab75bf3b38fa4fb974c790fa3b9d
ClemLB <clem.leborgne@gmail.com>
2019-12-20 08:57:44 +0100

    Exemple pour le main

--- EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
+++ EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
@@ -1,15 +1,45 @@
 package fr.eseo.i2.ld.edt;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	// Déclaration des variables
+	private Stage premierStage;
+	private BorderPane rootLayout;
+	
 	// Appellé quand le Main est lancé
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+		this.premierStage = primaryStage;
+		this.premierStage.setTitle("EMR Editor");
+		initRootLayout();
+	}
+	
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loaderRoot = new FXMLLoader(getClass().getResource("/vue/RootLayout.fxml"));
+			rootLayout = (BorderPane) loaderRoot.load();
+
+			// Give the controller access to the main class
+			//this.rootLayoutController = loaderRoot.getController();
+			//this.rootLayoutController.setMainClass(this);
+
+			// Show the scene containing the root layout.
+			Scene sceneRoot = new Scene(rootLayout);
+			premierStage.setScene(sceneRoot);
+			premierStage.show();
+
+		} catch (IOException e) {
+			System.out.println("Zut");
+		}
 	}
 
 }
