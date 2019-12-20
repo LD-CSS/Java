24ba702f700360d60651b0cd29917170c090cb59
ClemLB <clem.leborgne@gmail.com>
2019-12-20 09:35:30 +0100

    Début de quelque chose sur le RootLayout

--- EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
+++ EDT/src/main/java/fr/eseo/i2/ld/edt/Main.java
@@ -2,6 +2,7 @@ package fr.eseo.i2.ld.edt;
 
 import java.io.IOException;
 
+import fr.eseo.i2.ld.edt.controleur.RootLayoutController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,32 +11,43 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	// Déclaration des constantes
+	private final static String cheminVersVues = "/fr/eseo/i2/ld/edt/vue/";
+
 	// Déclaration des variables
 	private Stage premierStage;
 	private BorderPane rootLayout;
-	
+
+	// Controleurs
+	private RootLayoutController rootLayoutController;
+
+	public static void main(String[] args) {
+		launch();
+	}
+
 	// Appellé quand le Main est lancé
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.premierStage = primaryStage;
-		this.premierStage.setTitle("EMR Editor");
+		this.premierStage.setTitle("Emploi du temps");
+		
 		initRootLayout();
 	}
-	
+
 	public void initRootLayout() {
 		try {
 			// Load root layout from fxml file.
-			FXMLLoader loaderRoot = new FXMLLoader(getClass().getResource("/vue/RootLayout.fxml"));
+			FXMLLoader loaderRoot = new FXMLLoader(getClass().getResource(cheminVersVues + "RootLayout.fxml"));
 			rootLayout = (BorderPane) loaderRoot.load();
 
 			// Give the controller access to the main class
-			//this.rootLayoutController = loaderRoot.getController();
-			//this.rootLayoutController.setMainClass(this);
+			this.rootLayoutController = loaderRoot.getController();
+			this.rootLayoutController.setMain(this);
 
 			// Show the scene containing the root layout.
 			Scene sceneRoot = new Scene(rootLayout);
-			premierStage.setScene(sceneRoot);
-			premierStage.show();
+			this.premierStage.setScene(sceneRoot);
+			this.premierStage.show();
 
 		} catch (IOException e) {
 			System.out.println("Zut");
--- EDT/src/main/java/fr/eseo/i2/ld/edt/controleur/RootLayoutController.java
+++ EDT/src/main/java/fr/eseo/i2/ld/edt/controleur/RootLayoutController.java
@@ -1,5 +1,29 @@
 package fr.eseo.i2.ld.edt.controleur;
 
+import fr.eseo.i2.ld.edt.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class RootLayoutController {
 
+	// Déclaration des composants de la fenêtre
+	@FXML
+	private Button boutonExemple;
+
+	// Référence à l'application principale
+	private Main main;
+
+	public void setMain(Main main) {
+		this.main = main;
+	}
+
+	@FXML
+	private void initialize() {
+		System.out.println("La fenêtre principale a été initialisée");
+	}
+
+	@FXML
+	private void handleExemple() {
+		System.out.println("Appui sur le bouton exemple détecté !");
+	}
 }
